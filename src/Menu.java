c3c13b70a9a7471c919fe1244a2dbd4ff06eddfa
alexgani <alex.gani@menloschool.org>
2023-04-23 17:22:32 -0700

    outline

---
+++ src/Menu.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+
+public class Menu {
+    private window w;
+    private Game l;
+
+    public Menu() {
+
+    }
+
+    public void choice() {
+
+    }
+
+    public void paint(Graphics g) {
+
+    }
+}
